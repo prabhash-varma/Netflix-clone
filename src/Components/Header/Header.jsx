9a3562afebf3d175633ca4565150cb9a6a6bc4da
Prabhash Varma <varmaprabhash@gmail.com>
2022-12-24 18:42:22 +0530

    Update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../../logo.png"
+import logo from "../../logo2.png"
 import { Link } from "react-router-dom"
 import { ImSearch } from "react-icons/im"
 
